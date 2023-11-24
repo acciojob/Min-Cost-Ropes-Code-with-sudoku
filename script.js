0c335ab7d5ad8929f903cb2537477aff6cc709c1
Code-with-sudoku <137153314+Code-with-sudoku@users.noreply.github.com>
2023-11-24 12:30:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+// js part
+ 
+ 
+function compare(a, b) {
+  if (a < b) {
+      return -1;
+  } else if (a > b) {
+      return 1;
+  } else {
+      return 0;
+  }
 }
-
-module.exports=mincost;
